b7a4f4a2d3226f289b7140c9414143d8ef92c996
X.Y.Z <g.for.xyz@gmail.com>
2020-11-28 22:52:54 +0800

    chore: update deps and add `yarn get-help` script

--- scripts/print-help.js
+++ scripts/print-help.js
@@ -1,6 +1,19 @@
 console.log(`
-Available scripts:
-    yarn release
+Usage: yarn <command>
+
+Commands:
+    help                    Show this help message.
+    start                   Build the project in watch mode,
+                                and then you can test at any time.
+    test                    Test with Jest.
+    build                   Build the whole project.
+    build --tscjs-only      Build the project, targetting tscjs only.
+    make-package            Make NPM package.
+    lint                    Lint with eslint.
+    release                 Lint, build, test, generate changlog, ...
+                                and finally release the package to NPM.
+    cz                      Commit changes.
+    changlog                Generate "CHANGLOG.md".
 
 🚩 Make sure you run the scripts in the / of this project, not in './scripts'.
 `);
